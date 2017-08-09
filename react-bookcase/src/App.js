826ecdfe9875067da6c531e22ba1a225a3dd62d3
Konnor Welsch <kwelsch@2ndwatch.com>
2017-08-08 18:37:10 -0700

    Started with React Router

--- react-bookcase/src/App.js
+++ react-bookcase/src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './styles/App.css';
 import './styles/font-awesome-4.7.0/css/font-awesome.min.css';
+
 import { NavHeader, Content } from './containers';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavHeader />
-        <Content />
-      </div>
+      <Router>
+        <div className="App">
+          <NavHeader />
+          <Content />
+        </div>
+      </Router>
     );
   }
 }
